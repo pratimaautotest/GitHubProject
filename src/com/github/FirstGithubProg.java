e4eb430739e41509731fc714af7cc32a86cd56c1
lenovo <lenovo@LAPTOP-1SIGKS8P>
2023-06-20 23:06:24 +0530

    first commit...

---
+++ src/com/github/FirstGithubProg.java
@@ -0,0 +1,19 @@
+package com.github;
+
+public class FirstGithubProg {
+
+	public static void main(String[] args) {
+		
+			String hungry="yes";
+			
+			if(hungry == "yes")
+			{
+				System.out.println("Eat something");
+			}
+			else
+			{
+				System.out.println("Do work...");
+			}
+	}
+
+} 
